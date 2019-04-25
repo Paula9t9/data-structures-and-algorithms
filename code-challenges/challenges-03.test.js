86db521c7201286302a8b395d4892c456e45c7aa
Paula Thomas <paula9t9@gmail.com>
2019-04-24 17:23:34 -0700

    Solved problem 6

--- code-challenges/challenges-03.test.js
+++ code-challenges/challenges-03.test.js
@@ -93,7 +93,16 @@ For example, [1, 14, 0.2, -281, 54782] is only correctly sorted in that order.
 ------------------------------------------------------------------------------------------------ */
 
 const sortNumbersByLength = (arr) => {
-  // Solution code here...
+  arr.sort((a,b) => {
+    if(a.toString().length < b.toString().length){
+      return -1;
+    }else if(a.toString().length === b.toString().length){
+      return 0;
+    }else {
+      return 1;
+    }
+  });
+  return arr;
 };
 
 /*-----------------------------------------------------------------------------------------------
