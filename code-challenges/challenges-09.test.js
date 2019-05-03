8d1ea6a9f216fef29ff251f103e783d62d49869a
Paula Thomas <paula9t9@gmail.com>
2019-05-02 17:37:06 -0700

    solved number 2

--- code-challenges/challenges-09.test.js
+++ code-challenges/challenges-09.test.js
@@ -86,7 +86,10 @@ Note: You must use reduce for this challenge. You may not use the built-in .reve
 ------------------------------------------------------------------------------------------------ */
 
 const reversedString = (arr) => {
-  // Solution code here...
+  return arr.reduce((ansSoFar, currentValue) => {
+    ansSoFar.push(currentValue.pop());
+    return ansSoFar;
+  }, []);
 };
 
 /* ------------------------------------------------------------------------------------------------
