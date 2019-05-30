361f966c62140193eaa7a00fe462cc8893374918
Paula Thomas <paula9t9@gmail.com>
2019-05-30 09:14:41 -0700

    All binary tests passed

--- code401Challenges/src/test/java/code401Challenges/BinarySearchTest.java
+++ code401Challenges/src/test/java/code401Challenges/BinarySearchTest.java
@@ -17,5 +17,32 @@ public class BinarySearchTest {
 
     }
 
-    @Test 
+    @Test
+    public void testBinarySearch_testEvenArray(){
+        BinarySearch binarySearch = new BinarySearch();
+        int[] inputArray = new int[]{1, 2, 3, 4, 5, 6};
+        int findNumber = 2;
+        int firstExpectedResult = 1;
+
+        assertEquals("Should return index for found number",
+                firstExpectedResult, binarySearch.myBinarySearch(inputArray, findNumber));
+
+        int findSecondNumber = 3;
+        int secondExpectedResult = 2;
+        assertEquals("Should return the index where the number was found",
+                secondExpectedResult, binarySearch.myBinarySearch(inputArray,findSecondNumber));
+
+    }
+
+    @Test
+    public void testBinarySearch_testGapArray(){
+        BinarySearch binarySearch = new BinarySearch();
+        int[] inputArray = new int[]{1, 3, 4, 5, 6, 9, 22, 33};
+        int findNumber = 9;
+        int expectedResult = 5;
+
+        assertEquals("Should return the index where the number was found\"r",
+                expectedResult, binarySearch.myBinarySearch(inputArray, findNumber));
+
+    }
 }
