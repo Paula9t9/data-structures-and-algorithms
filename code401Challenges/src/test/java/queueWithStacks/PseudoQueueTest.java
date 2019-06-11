c763155a3f1603321f72bba9bc3de40d5ad43a1c
Paula Thomas <paula9t9@gmail.com>
2019-06-10 18:21:25 -0700

    Added new tests

--- code401Challenges/src/test/java/queueWithStacks/PseudoQueueTest.java
+++ code401Challenges/src/test/java/queueWithStacks/PseudoQueueTest.java
@@ -29,7 +29,7 @@ public class PseudoQueueTest {
     }
 
     @Test
-    public void enqueue() {
+    public void testEnqueue_basic() {
 
         assertEquals("Values should be properly added", expectedList.getListOfNodes(),
                 testQ.getPsuedoQueueNodes());
@@ -37,7 +37,7 @@ public class PseudoQueueTest {
 
 
     @Test
-    public void dequeue() {
+    public void testDequeue_basic() {
 
         expectedList = new LinkedList();
         expectedList.insert(2);
@@ -47,6 +47,59 @@ public class PseudoQueueTest {
 
         assertEquals("Should remove the first value added to the queue", 1, testQ.dequeue());
         assertEquals("Value should no longer be in the queue", expectedList.getListOfNodes(),
+                testQ.getPsuedoQueueNodes());
+    }
+
+    @Test
+    public void testEnQAndDeQ(){
+
+        testQ.dequeue();
+        testQ.dequeue();
+        testQ.dequeue();
+        testQ.dequeue();
+        testQ.dequeue();
+
+        testQ.enqueue(6);
+        testQ.enqueue(7);
+        testQ.enqueue(8);
+        testQ.enqueue(9);
+        testQ.enqueue(10);
+
+        expectedList = new LinkedList();
+        expectedList.insert(6);
+        expectedList.insert(7);
+        expectedList.insert(8);
+        expectedList.insert(9);
+        expectedList.insert(10);
+
+        assertEquals("Should properly remove all values and add new values", expectedList.getListOfNodes(),
+                testQ.getPsuedoQueueNodes());
+    }
+
+    @Test
+    public void testEnQAndDeQ_halfEmpty(){
+
+        testQ.dequeue();
+        testQ.dequeue();
+        testQ.dequeue();
+
+        testQ.enqueue(6);
+        testQ.enqueue(7);
+        testQ.enqueue(8);
+        testQ.enqueue(9);
+        testQ.enqueue(10);
+
+        expectedList = new LinkedList();
+        expectedList.insert(4);
+        expectedList.insert(5);
+        expectedList.insert(6);
+        expectedList.insert(7);
+        expectedList.insert(8);
+        expectedList.insert(9);
+        expectedList.insert(10);
+
+        assertEquals("Should properly add new values even after some values have been removed",
+                expectedList.getListOfNodes(),
                 testQ.getPsuedoQueueNodes());
     }
 }
