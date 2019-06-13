75e3fb4a8d8bd30a76e9a24c693f41233aa64fa9
Paula Thomas <paula9t9@gmail.com>
2019-06-12 18:56:33 -0700

    Started building out, need to generify nodes and queues

--- code401Challenges/src/main/java/fifoAnimalShelter/AnimalShelter.java
+++ code401Challenges/src/main/java/fifoAnimalShelter/AnimalShelter.java
@@ -1,19 +1,34 @@
 package fifoAnimalShelter;
 
 import java.util.ArrayList;
+import java.util.Map;
+import java.util.Queue;
 
 public class AnimalShelter {
 
-    public void donate(Cat cat){}
+    private Queue<Cat> catQueue;
+    private Queue<Dog> dogQueue;
+    //Got map idea from class
+    private Map<Animal, Integer> arrivalOrder;
 
-    public void donate(Dog cat){}
+    public AnimalShelter(){
+        this.catQueue = new Queue<>();
+    }
+
+    public void donate(Cat cat){
+        catQueue.add(cat);
+    }
+
+    public void donate(Dog dog){
+        dogQueue.add(dog);
+    }
 
     public Cat adoptCat(){
-        return new Cat("default", "default", 10);
+        return (Cat) catQueue.remove();
     }
 
     public Dog adoptDog(){
-        return new Dog("default", "default", 10);
+        return (Dog) dogQueue.remove();
     }
 
     public Animal adoptAny(){
