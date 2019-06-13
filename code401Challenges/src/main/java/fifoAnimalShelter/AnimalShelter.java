8a877c23610870fc63ae8f53d6f6ac7913425847
Paula Thomas <paula9t9@gmail.com>
2019-06-12 17:43:34 -0700

    added skeleton methods

--- code401Challenges/src/main/java/fifoAnimalShelter/AnimalShelter.java
+++ code401Challenges/src/main/java/fifoAnimalShelter/AnimalShelter.java
@@ -1,6 +1,26 @@
 package fifoAnimalShelter;
 
+import java.util.ArrayList;
+
 public class AnimalShelter {
 
+    public void donate(Cat cat){}
 
+    public void donate(Dog cat){}
+
+    public Cat adoptCat(){
+        return new Cat("default", "default", 10);
+    }
+
+    public Dog adoptDog(){
+        return new Dog("default", "default", 10);
+    }
+
+    public Animal adoptAny(){
+        return new Cat("default", "default", 10);
+    }
+
+    public ArrayList<Animal> getResidents(){
+        return new ArrayList<>();
+    }
 }
