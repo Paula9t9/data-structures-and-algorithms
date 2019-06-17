ea9b904c4d145f2e02127faec7e87459f29e4bae
Paula Thomas <paula9t9@gmail.com>
2019-06-17 09:48:27 -0700

    Initialized tree classes

---
+++ code401Challenges/src/main/java/tree/TreeNode.java
@@ -0,0 +1,49 @@
+package tree;
+
+public class TreeNode<T> {
+
+    private T value;
+    private TreeNode leftChild;
+    private TreeNode rightChild;
+
+
+    public TreeNode() {
+        value = null;
+        leftChild = null;
+        rightChild = null;
+    }
+
+    public TreeNode(T value) {
+        this.value = value;
+    }
+
+    public TreeNode(T value, TreeNode leftChild, TreeNode rightChild) {
+        this.value = value;
+        this.leftChild = leftChild;
+        this.rightChild = rightChild;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
+
+    public TreeNode getLeftChild() {
+        return leftChild;
+    }
+
+    public void setLeftChild(TreeNode leftChild) {
+        this.leftChild = leftChild;
+    }
+
+    public TreeNode getRightChild() {
+        return rightChild;
+    }
+
+    public void setRightChild(TreeNode rightChild) {
+        this.rightChild = rightChild;
+    }
+}
