dc7bd086fb83926ee4964553a772a6458898f5f6
Paula Thomas <paula9t9@gmail.com>
2019-07-08 12:30:58 -0700

    Initial scaffolding for hash class

---
+++ code401Challenges/src/main/java/hashTable/HashTable.java
@@ -0,0 +1,19 @@
+package hashTable;
+
+public class HashTable {
+
+    public void add(String key, String value){
+    }
+
+    public String get(String key){
+        return null;
+    }
+
+    public boolean contains(String key){
+        return false;
+    }
+
+    public int hash(String key){
+        return 0;
+    }
+}
