aad00fffbca18bb92dea5323dd42a99ee825d3da
Paula Thomas <paula9t9@gmail.com>
2019-07-18 10:09:50 -0700

    passes basic test

---
+++ code401Challenges/src/test/java/depthFirst/DepthFirstTest.java
@@ -0,0 +1,28 @@
+package depthFirst;
+
+import graph.Graph;
+import graph.GraphNode;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DepthFirstTest {
+
+    @Test
+    public void breadthFirst() {
+        Graph testGraph = new Graph();
+        GraphNode firstNode =  testGraph.addNode("A");
+        GraphNode bNode = testGraph.addNode("B");
+        GraphNode cNode =testGraph.addNode("C");
+        GraphNode dNode =testGraph.addNode("D");
+
+        testGraph.addEdge(firstNode, bNode, 1);
+        testGraph.addEdge(bNode, cNode, 1);
+        testGraph.addEdge(bNode, dNode, 1);
+
+        String expectedString = "ABDC";
+
+        assertEquals("Should put all values from graph into a string", expectedString,
+                DepthFirst.depthFirst(firstNode));
+    }
+}
